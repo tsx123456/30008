7fd2e650bbd7c2e6f0aa9a7242877795125dd5d7
上帝的梦 <1597691532@qq.com>
2025-08-28 18:47:10 +0800

    1

--- index.d5d28.js
+++ index.d5d28.js
@@ -1,4 +1,4 @@
-System.register(["./application.abef6.js"], function (_export, _context) {
+System.register(["./application.161b5.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
