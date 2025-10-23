ba6561d2067228326592bc6fe2bb0fc95c24e6ea
上帝的梦 <1597691532@qq.com>
2025-10-23 15:49:15 +0800

    1

--- index.4a3e4.js
+++ index.4a3e4.js
@@ -1,4 +1,4 @@
-System.register(["./application.dcfd0.js"], function (_export, _context) {
+System.register(["./application.bd728.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
